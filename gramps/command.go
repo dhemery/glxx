41ddedf2a1aa962892ed4363787fc7dda1e5dcb0
Dale Emery <dale@dhemery.com>
2026-09-10 18:12:24 -0700

    Write converted files to disk

--- gramps/command.go
+++ gramps/command.go
@@ -4,6 +4,8 @@ package gramps
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"slices"
 
 	"github.com/dhemery/glxx/dump"
 	"github.com/genealogix/glx/go-glx"
@@ -11,13 +13,11 @@ import (
 )
 
 var (
-	importDumpGLX = false
-	importDumpXML = false
+	importDump = ""
 )
 
 func init() {
-	Command.Flags().BoolVar(&importDumpXML, "dumpgramps", importDumpXML, "Dump loaded Gramps data to JSON and exit")
-	Command.Flags().BoolVar(&importDumpGLX, "dumpglx", importDumpGLX, "Dump converted GLX to JSON and exit")
+	Command.Flags().StringVar(&importDump, "dump", importDump, "Dump gramps, glx, or files to JSON and exit")
 }
 
 var Command = &cobra.Command{
@@ -27,13 +27,18 @@ var Command = &cobra.Command{
 	RunE:  run,
 }
 
+var dumpTypes = []string{"gramps", "glx", "files"}
+
 func run(c *cobra.Command, args []string) error {
+	if importDump != "" && !slices.Contains(dumpTypes, importDump) {
+		return fmt.Errorf("invalid dump type %q: expecting one of %s", importDump, dumpTypes)
+	}
 	raw, err := loadGrampsXML(args[0])
 	if err != nil {
 		return err
 	}
 
-	if importDumpXML {
+	if importDump == "gramps" {
 		return dump.WriteJSON(os.Stdout, raw)
 	}
 
@@ -43,20 +48,31 @@ func run(c *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-	if importDumpGLX {
+	if importDump == "glx" {
 		return dump.WriteJSON(os.Stdout, glxFile)
 	}
 
 	s := glx.NewSerializer(nil)
 
-	serialized, err := s.SerializeMultiFileToMap(glxFile)
+	files, err := s.SerializeMultiFileToMap(glxFile)
 	if err != nil {
 		return err
 	}
 
-	for f, c := range serialized {
-		fmt.Fprintln(os.Stdout, f, string(c))
+	if importDump == "files" {
+		for f, c := range files {
+			fmt.Fprintln(os.Stdout, f)
+			fmt.Fprintln(os.Stdout, string(c))
+		}
 	}
 
+	for f, c := range files {
+		if err := os.MkdirAll(filepath.Dir(f), 0755); err != nil {
+			return err
+		}
+		if err := os.WriteFile(f, c, 0644); err != nil {
+			return err
+		}
+	}
 	return nil
 }
